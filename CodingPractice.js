815addc355d29d9b90786b818e8ba236cf58b98d
is.dmytro.kuzmenko <maximus19919@gmail.com>
2024-11-11 09:09:09 -0800

    Algorytms Week 4.1

--- CodingPractice.js
+++ CodingPractice.js
@@ -11,3 +11,20 @@
 // }
 
 // console.log(counter(10, ["call","call","call"]));
+
+const array = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [44, 56],
+  [77, 95],
+];
+let result = 0;
+
+for (let i = 0; i < array.length; i++) {
+  const sum = array[i][0] + array[i][1];
+  if (sum > result) {
+    result = sum;
+  }
+}
+console.log(result);
