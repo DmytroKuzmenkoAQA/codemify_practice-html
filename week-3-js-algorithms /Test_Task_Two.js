bce20ac1d714f3d0f47ef4f0f042646dc7475187
is.dmytro.kuzmenko <maximus19919@gmail.com>
2024-12-20 15:16:55 -0800

    Remove unnecessary files

--- week-3-js-algorithms /Test_Task_Two.js
+++
@@ -1,8 +0,0 @@
-// 2 - Create a function that returns the length of a string
-
-function getStringLength(str) {   
-    return str.length;
-  }
-  
-  console.log(getStringLength('sergii')) // 6
-  console.log(getStringLength('alex')) // 4
